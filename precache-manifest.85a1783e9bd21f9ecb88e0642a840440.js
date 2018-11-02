bae5319531ff380f538f40778ba21edb985ba2d6
Jesus Jimenez <jjimenez@itexico.net>
2018-11-02 16:40:30 -0600

    Updates

---
+++ precache-manifest.85a1783e9bd21f9ecb88e0642a840440.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "95691abbdb9c9adcee33",
+    "url": "/static/js/main.95691abb.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "95691abbdb9c9adcee33",
+    "url": "/static/css/main.0f56c5d2.chunk.css"
+  },
+  {
+    "revision": "2e54627073a485f9d6fab49a53f9e94d",
+    "url": "/index.html"
+  }
+];
